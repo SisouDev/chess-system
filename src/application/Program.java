42d6d48408078f3191e016e352a26dd885d423e0
Maria Karolina <karolinasingulani2@gmail.com>
2022-10-27 15:37:03 -0300

    first class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
-import tabuleiro.Position;
+import boardgame.Board;
 
 public class Program {
     public static void main(String[] args){
-        Position po = new Position(3, 5);
-        System.out.println(po);
+        Board board = new Board(8, 8);
+
     }
 }
